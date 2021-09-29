cbca7fec0bdb4e49dae0325b3d9d80648c0c77db
Eduardo Ferreira Aquino <eduardoferreira7@gmail.com>
2021-09-29 00:56:16 -0300

    first commit

--- test.py
+++ test.py
@@ -9,10 +9,12 @@ def temperature():
     r = requests.get('http://api.openweathermap.org/data/2.5/weather?zip='+zipcode+',us&appid=79454fcdc4618cae64f45852c90a80b1')
     json_object = r.json()
     temp_k = float(json_object['main']['temp'])
+    humidity_py = float (json_object['main']['humidity'])
+    country_py = json_object['sys']['country']
     #temp_f = float((temp_k - 273.15)) * 1.8 + 32
     temp_c = float(temp_k-273)
 
-    return render_template('temperature.html', temp=temp_c)
+    return render_template('temperature.html', temp=temp_c, humidity=humidity_py, country = country_py)
 
 @app.route('/')
 def index():
